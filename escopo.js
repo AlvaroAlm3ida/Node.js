bc06e401c792c4a4aa0946a25b560b56f3ecb808
AlvaroAlm3ida <alvarosantosalmeida120@gmail.com>
2025-04-06 01:02:47 -0300

    começando

---
+++ escopo.js
@@ -0,0 +1,8 @@
+if(1 > 0){
+    let estudade = 'Caroline';
+    console.log(estudade);
+}
+
+// Nessa parte aqui é como se o javascript tivesse adicionado um VAR não sendo a mesma variável declarada no bloco if.
+estudade = 'ana';
+console.log(estudante);
