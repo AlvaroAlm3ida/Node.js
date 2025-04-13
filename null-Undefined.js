b22215ade2c8002497ec2d0cf4443676276ac97b
AlvaroAlm3ida <alvarosantosalmeida120@gmail.com>
2025-04-13 14:30:31 -0300

    Finalizando módulo

---
+++ null-Undefined.js
@@ -0,0 +1,12 @@
+let nomeEstudante;
+const professora = 'ana';
+
+// typeof utilizamos para saber qual tipo há dentro da variável ou const
+console.log(typeof nomeEstudante);
+console.log(typeof professora);
+
+let telefoneEstudante = null;
+
+console.log(typeof telefoneEstudante);
+
+console.log(telefoneEstudante + 3);
