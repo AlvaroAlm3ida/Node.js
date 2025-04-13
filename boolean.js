dd7ddd104dda7b3e2209f5890bcea75dd5aa8552
AlvaroAlm3ida <alvarosantosalmeida120@gmail.com>
2025-04-13 01:30:13 -0300

    tipo boolean

---
+++ boolean.js
@@ -0,0 +1,12 @@
+const estudate = 'Fernando';
+const notaEstudate = true;
+
+if (notaEstudate === true){
+    console.log("Voce passou, boas festas");
+}else{
+    console.log("REPROVADO, boas festas");
+}
+
+if(estudate === 'Fernando'){
+    console.log(`Olá ${estudate}!`);
+}
